a9615d4a581d0a3e52d1a65b2b283424da36253d
Hrishikesh Patkar <patkarhrishikesh0204@gmail.com>
2023-11-15 11:11:10 +0530

    15nov commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,21 @@ import contacts from "../contacts";
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
+      <h1 className="heading">DIWALI FARAL</h1>
       <Card
         name={contacts[0].name}
         img={contacts[0].img}
         tel={contacts[0].tel}
         mail={contacts[0].mail}
       />
+
+      <Card 
+        name={contacts[1].name}
+        img={contacts[1].img}
+        tel={contacts[1].tel}
+      />
+
+      
     </div>
   );
 }
--- src/contacts.js
+++ src/contacts.js
@@ -1,17 +1,15 @@
 const contacts = [
   {
-    name: "Hrishi",
+    name: "chakli",
     img:
-      "https://lh3.googleusercontent.com/pw/ADCreHdOy21aisyXSlXInIukN3RMrWDCfQfnJJETPGhOOmXbMRlBvZCSk0NG_YMsXPU7tQrX-WWcKMWHnFJPPx-aDuCFdzEkCrkgLDZgSFoHaFhtb6GoF7PxhVB_ebHvZOx-0n9bnZ0jcgYvDrRTy0ELYp3OZHuWMSjTr22LplgoIhVyTP5jASckmo7VkBA3FmYRWQIat1t9vVPmxlEO1uG0xTiJ_GiXM3P2c4K9rM6h6CJJwDH8CHvAViiTuBeSRwRpaGFxUZlko6_1yb3vtrllgu1yCrewviAVwr-EHcAmLCh8Ykzh9qgM8B-E7WiImKdIU2wezbuRIj83Oc6uokUl57WuE5vPrWWcodARwTDGsHQhhrBIGPtPK7cIql_7lrtJQZ5l26VLSNojrEGSy4ROhZvzxclNPOlb7jgtm27cF6DUtIt9nSV4hZ0hlAuU795P0LAjE-wCy_wjw6c919vXwzBC5zsWzkl-wezFeOYbKFnV-rxm3UtDCi5GCbwpj-z-H9qxH_S5pRaSdQNYEgjwTT8SP1tG6c6tKLsX5xqCsxLWQVtegoV5Kmibzn5R9OrN0qkcqsXCcKHH4pGWcFOV1XjVFfQZF7NE_jzoXXiMARPPb9cxJCnTEp152UTJhf7fjpIsOnQZtyN3-WFbqr6ka1e4hOzT4X9ZNLyU5oDa12ikoFQbcSCaO945vU-VuHdazap3zLRJIXZgBV2oMFxla72rSPxeOyBskNNeuWeo3yRlrGgrb0dyauiXlIfK7lNJxwR03wYdSpMmf8zEPswKA1oCD4Cl6dzpj0J7BXUU5y0Nt59C2G2QqV0PcY-mfPwEAXov58Y-gzT5w8_5_9DCi08BDf3eEECmijLenRuMG7B0WdPVf-KOXJU54uwTm_h58opCLVMZw8UPd1SDtDYCHLh8OfCenArOj264SF9Fv0gC_gzawJneZscELpoNGw_VTaod1sAegwz8X17jLRU8t580qdfqBqo=w627-h837-s-no-gm?authuser=0",
-    tel: "+325896",
-    mail: "once.com"
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Chakli_in_a_bowl.jpg/1200px-Chakli_in_a_bowl.jpg",
+    tel: "Chakli is a savoury snack from India. It is a spiral shaped snack with a spiked surface.",
   },
   {
-    name: "Jack Bauer",
-    imgURL:
-      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
-    phone: "+987 654 321",
-    email: "jack@nowhere.com"
+    name: "chiwda",
+    img:
+      "https://upload.wikimedia.org/wikipedia/commons/3/3f/Bowl_of_Chivda.jpg",
+    tel: " Indian snack mix (namkeen) which consists of a variable mixture of spicy dried ingredients, such as fried lentils, peanuts."
   },
   {
     name: "Chuck Norris",
